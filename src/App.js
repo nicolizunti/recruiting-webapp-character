7b6337b7104f2d9963827d470d451437cc96d7b4
Nicoli Martins <57541357+nicolizunti@users.noreply.github.com>
2024-02-17 00:08:06 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import { useState } from 'react';
-import './App.css';
-import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
-
+import { useState } from "react";
+import "./App.css";
+import CharacterSheet from "./CharacterSheet";
 
 function App() {
-  const [num, setNum] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Coding Exercise</h1>
+        <h1>My Character Sheet</h1>
       </header>
-      <section className="App-section">
+      <section className="character-menu">
         <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
+          <button>Add New Character</button>
+          <button>Reset All Charactert</button>
+          <button>Save All Charactert</button>
         </div>
       </section>
+      <section className="skill-check-display">
+        <h2>Skill Check Results</h2>
+        <h3>Character 1</h3>
+        <p>
+          Skill: {"skill name"}: {"# skill Point"}
+        </p>
+        <p>You Rolled: {"dice #"}</p>
+        <p>The DC was: {"DC #"}</p>
+        <p>Result: {"result"}</p>
+      </section>
+      <CharacterSheet />
     </div>
   );
 }
